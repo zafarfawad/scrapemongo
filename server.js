16ee12a164b8f8f1492ddf610cdf073b884fe1ba
Fawad Zafar <fawad.zafar@robotsandpencils.com>
2018-03-17 15:36:37 -0400

    dd

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3004;
+// var PORT = 3004;
+var PORT = process.env.PORT || 3004;
 
 // Initialize Express
 var app = express();
