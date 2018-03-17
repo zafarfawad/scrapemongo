d7313abfa040bc42a4ac21a93a590b8aa3617a84
Fawad Zafar <fawad.zafar@robotsandpencils.com>
2018-03-17 18:31:37 -0400

    add password

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ mongoose.Promise = Promise;
 // mongoose.connect("mongodb://localhost/week18Populaterfawad17", function (err){
     // mongoose.connect("mongodb://heroku_hv90brj8:ar6kop96gn2v8bs8qsgo36677c@ds117489.mlab.com:17489/heroku_hv90brj8",{
 
-mongoose.connect("mongodb:heroku_x1zwt4p4:Password1@ds117509.mlab.com:17509/heroku_x1zwt4p4", function (err){
+mongoose.connect("mongodb:heroku_x1zwt4p4:Password1!@ds117509.mlab.com:17509/heroku_x1zwt4p4", function (err){
     if(err) throw err;
 });
 
